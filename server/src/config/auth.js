4b9381cc1ce609e53252c6229a5704d10a127388
SMarco2310 <marc.sossou@ashesi.edu.gh>
2026-05-01 23:04:16 +0000

    refactor: update auth config to remove hardcoded localhost fallback and add local origins to trusted list

--- server/src/config/auth.js
+++ server/src/config/auth.js
@@ -24,12 +24,12 @@ const sendEmail = async ({ to, subject, text, html }) => {
 };
 
 export const auth = betterAuth({
-  baseURL: process.env.BETTER_AUTH_URL || "http://localhost:4000/api/auth",
+  baseURL: process.env.BETTER_AUTH_URL,
+
   trustedOrigins: [
-    // "http://localhost:5173", 
-    // "http://localhost:5174", 
-    // "http://localhost:4000", 
-    // "http://localhost:3000",
+    "http://localhost:5173",  
+    "http://localhost:4000", 
+
     "https://asthma-guard.vercel.app",
     "https://asthmaguard.onrender.com"
   ],
